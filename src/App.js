0da15136f724a87598638cd0afdba03405c0f621
Neha Gholap <nehagholap98@gmail.com>
2024-05-18 12:03:20 +0530

    react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+function App() 
+{
+  const [dog, setdog] = useState ({message : '', status : ''});
+
+  useEffect (() =>
+  {
+    /*alert ('Hello world !'); */
+    getData();
+  }, [])
+
+  async function getData()
+  {
+    const res = await fetch ('https://dog.ceo/api/breeds/image/random');
+    const data = await res.json ();
+    console.log (data);
+    setdog (data);
+  }
+  const [msg, setmsg] = useState ('Marvellous Infosystems');
+  const [btn, setbtn] = useState ('Subscribe');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{msg}</h1>
+      <button onClick={()=>{setmsg ('Welocome To Marvellous Infosystems Family .'); setbtn ('Subscribed')}}>{btn}</button>
+    <div className='imagecontainer'>
+      <img src = {dog.message} alt = 'dog' style={{width : '60%', borderRadius : '60px'}}/>
+    </div>
     </div>
   );
 }
